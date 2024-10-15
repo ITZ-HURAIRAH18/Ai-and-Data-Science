f00851227823a37c4eb385d26f0b331cbaee7e9f
ITZ-HURAIRAH18 <muhammadabuhurairah22@gmail.com>
2024-10-15 11:22:30 +0500

    del lect5

--- 3)python/1) Introduction/hello.py
+++
@@ -1,9 +0,0 @@
-
-# Purposes:
-
-# Brief overview of Python: High-level, interpreted, and general-purpose language.
-# Uses of Python: Web development, automation, data science, AI, and more.
-# Discuss Python's popularity due to its simplicity and large community support.
-
-
-print("hello world !!")
